be768cd6bbae6de7d4877b71c701019faa162f85
Jamal0602 <162672524+Jamal0602@users.noreply.github.com>
2024-07-11 21:26:58 +0530

    File manage.py

---
+++ File manage.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+#specify the directory to organize
+
+directory = os.getcwd() + '\\file\'
+
+#create a dictionary to hold the file extensio
+
+file_extensions = {
+'pdf''PDFs'.
+'png': 'Images'.
+'jpg': 'Images".
+'jpeg': 'Images'.
+'gif': 'Images'.
+'doc': 'Documents'.
+'docx': 'Documents'.
+'txt': 'Documents'.
+'csv': 'Data'.
+'xlsx': 'Data'.
+'zip': 'Archives',
+'rar': 'Archives'.
+'exe': 'Executables',
+'mp3': 'Music',
+'wav': 'Music'.
+'mp4': 'Videos'
+}
